82aa0ea8e80604e3c0aafbf47a496cfaf37d0f32
Amos Newton <2667821n@student.gla.ac.uk>
2024-03-04 21:50:45 +0000

    added urls, views for about page. Fixed errors with loading staticfiles.

--- artecho/urls.py
+++ artecho/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     # added for html test viewing:
     path('addroot', views.add_root, name='add_root'),
     path('card', views.card, name='card'),
+    path('login/', views.user_login, name='login'),
+    path('about/', views.about, name='about'),
     # html test urlpatters end here---
 ]
--- artecho/views.py
+++ artecho/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
 
 def index(request):
     context_dict = {'boldmessage': 'Welcome to ArtEcho!'}
@@ -12,3 +17,27 @@ def card(request):
 def add_root(request):
     return render(request, 'artecho/add-root.html')
 # html test views end here---
+
+def about(request):
+    context_dict = {'boldmessage': "This is about ArtEcho"}
+    print(request.method)
+    print(request.user)
+    return render(request, 'artecho/about.html', context=context_dict)
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('artecho:index'))
+            else:
+                return HttpResponse("Your artecho account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'artecho/login.html')

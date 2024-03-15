b48e91a5cbde45e703fcae10896445750f7cca2c
Yigit Sayar <yiit.sayar111@gmail.com>
2024-03-15 12:03:11 +0000

    Reverting back to the pre-merged State

--- artecho/views.py
+++ artecho/views.py
@@ -19,6 +19,12 @@ def card(request):
 
 def add_root(request):
     return render(request, 'artecho/add-root.html')
+
+def profile(request):
+    return render(request, 'artecho/profile.html')
+
+def profile_edit(request):
+        return render(request, 'artecho/profile-edit.html')
 # html test views end here---
 
 def about(request):
@@ -100,3 +106,4 @@ def add_root(request):
     else:
         form = ImageForm()
     return render(request, 'artecho/add-root.html', {'form': form})
+

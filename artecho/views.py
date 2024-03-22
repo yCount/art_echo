831253b5918f4e4b68866ce213f96a140c7b6687
Amos Newton <2667821n@student.gla.ac.uk>
2024-03-22 16:00:34 +0000

    cookies

--- artecho/views.py
+++ artecho/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import get_object_or_404
+from datetime import datetime
 
 def index(request):
     try:
@@ -98,6 +99,25 @@ def signup(request):
                            'profile_form': profile_form,
                            'registered': registered})
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days >= 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
 def user_login(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -109,7 +129,9 @@ def user_login(request):
             if user:
                 if user.is_active:
                     login(request, user)
-                    return redirect(reverse('artecho:index'))
+                    response = redirect(reverse('artecho:index'))
+                    response.set_cookie('auth_token', 'repsonse_token', max_age=3600)
+                    return response
                 else:
                     return HttpResponse("Your artecho account is disabled.")
             else:

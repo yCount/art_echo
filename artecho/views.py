54efbc38c7aaec3b39634f3e1c2fe33eaf32c7db
Amos Newton <2667821n@student.gla.ac.uk>
2024-03-22 12:08:28 +0000

    extended database

--- artecho/views.py
+++ artecho/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, Http404
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.shortcuts import redirect
